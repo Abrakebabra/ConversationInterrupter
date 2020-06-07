d0cc7cc57a36bf2f3e1fda6a9fcc04e19df51f0d
Keith Lee <lee.mingkeith@gmail.com>
2020-06-08 01:18:25 +0900

    Initial Commit

---
+++ ConversationInterrupter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConversationInterrupter
+//
+//  Created by Keith Lee on 2020/06/08.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
